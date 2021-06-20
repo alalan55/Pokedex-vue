a22cbfbd81b92bf1d93b7974854d53c51731aec2
Alan Oliveira <alla.ao4975@gmail.com>
2021-06-20 11:32:31 -0300

    Pokedex vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="elements">
+    <input type="text" class="pesquisar" placeholder="Pesquise um pokemon" />
+    <input type="submit" value="Pesquisar" />
+
+    <Cards />
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Cards from "@/components/Cards.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,  
+    Cards,
+  },
+};
 </script>
+
+<style scoped>
+.elements {
+  /* border: 1px solid; */
+  /* height: 100vh; */
+  display: flex;
+  flex-direction: column;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 2em;
+}
+.pesquisar {
+  width: 100%;
+}
+
+input[type="text"] {
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  display: block;
+  width: 100%;
+  padding: 10px 15px;
+  border-radius: 8px;
+  margin-bottom: 15px;
+
+  color: #333;
+  font-size: 18px;
+  box-shadow: 0px 0px 0px rgba(0, 0, 0, 0);
+  background-color: #f3f3f3;
+  transition: 0.4s;
+}
+input[type="text"]::placeholder {
+  color: #888;
+  transition: 0.4s;
+}
+input[type="submit"] {
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  display: block;
+  width: 100%;
+  padding: 10px 15px;
+  background-color: #1e1e1e;
+  border-radius: 8px;
+  color: #fff;
+  font-size: 18px;
+  font-weight: 700;
+  cursor: pointer;
+  margin-bottom: 25px;
+  transition: .2s ease-in-out;
+}
+input[type="submit"]:hover{
+  background-color: #3a3939;
+}
+
+input[type="submit"]:focus label {
+  color: #ea526f;
+}
+</style>
