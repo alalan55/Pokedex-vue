bcb5b0e77c050e855ba306969e46b891c345802f
aoliveira <aoliveira@keener.io>
2022-02-12 21:02:38 -0300

    Insere stats atualizado e ajustes no componente de fotos

--- src/components/organisms/dinamycsSteps/Stats.vue
+++ src/components/organisms/dinamycsSteps/Stats.vue
@@ -5,7 +5,10 @@
         <span class="title">
           {{ stat.stat.name }}
         </span>
-        <input
+
+        <div class="slider-content">
+           <input
+        class="slider"
           type="range"
           :name="stat.stat.name"
           id=""
@@ -15,6 +18,8 @@
         <span class="value-range">
           {{ stat.base_stat }}
         </span>
+        </div>
+       
       </div>
     </div>
   </div>
@@ -45,10 +50,56 @@ export default {
   border: 1px solid red;
   overflow: hidden;
 }
+.item{
+  margin: .5rem 0;
+}
+.item .title{
+  margin: .2rem 0;
+  display: block;
+  font-weight: 500;
+}
 .itens-galeria {
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.slider {
+  -webkit-appearance: none;
+  width: 80%;
+  height: 12px;
+  border-radius: 5px;
+  background: #d3d3d3;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: #04AA6D;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: #04AA6D;
+  cursor: pointer;
+}
+.value-range{
+  padding: 0 .5rem;
+  font-size: .8em;
 }
 </style>
 
